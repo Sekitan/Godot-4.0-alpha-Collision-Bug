6c3b0e73ec1f8f9b8eaf8e02b788ca25beabb6fa
Sekitan <sekitan@teknik.io>
2022-09-12 20:17:04 +0900

    initial commit

---
+++ player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	# Here's where the bug happens...
+	# Notes:
+	# - Setting BuggyCollision's Top Level on or off does nothing.
+	# - Changing global_position to position or vice-versa does nothing.
+	# - The CharacterBody3D appears to be on some invisible platform.
+	$BuggyCollision.global_position = self.global_position
+	print($BuggyCollision.global_position)
+	
+	
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
